c1403e3da85c0799f120efea98ced1166b5a1682
Victor Garric <victor.gairrc@gmail.com>
2021-01-14 23:11:10 +0100

    Init

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pip_search",
+    version="0.0.2",
+    author="Victor Garric",
+    author_email="victor.garric@gmail.com",
+    description="A package to search like pip used to via PyPi",
+    packages=setuptools.find_packages(),
+	install_requires=['tabulate'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.',
+	entry_points={
+        "console_scripts": [
+            "pip_search=pip_search.pip_search:search",
+        ],
+    },
+)
