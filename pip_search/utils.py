e60f3f1f16694a7ce9cf36fa22bc4668cfcf0be6
Victor Garric <victor.garric@gmail.com>
2022-01-03 09:38:07 +0100

    Update utils.py
    Compatibility for version < 3.8

--- pip_search/utils.py
+++ pip_search/utils.py
@@ -1,9 +1,21 @@
-from importlib.metadata import PackageNotFoundError
-from importlib.metadata import version as pkg_version
+from sys import version_info as py_ver
+
+if py_ver[0] == 3 and py_ver[1] >= 8:
+
+	from importlib.metadata import PackageNotFoundError
+	from importlib.metadata import version as pkg_version
 
 
-def check_version(package: str) -> str | None:
-    try:
-        return pkg_version(package)
-    except PackageNotFoundError:
-        return False
+	def check_version(package: str) -> str | None:
+		try:
+			return pkg_version(package)
+		except PackageNotFoundError:
+			return False
+
+else:
+	import pkg_resources as pkg
+	def check_version(package: str):
+		try:
+			return pkg.get_distribution(package).version
+		except :
+			return False
