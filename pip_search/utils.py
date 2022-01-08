d793ba239e2e86907f4db56b4b4ac76fd04f5c82
Rodrigo Cristiano <rcristianofv@hotmail.com>
2022-01-08 07:10:11 -0300

    refactor: Simplifies version checking

--- pip_search/utils.py
+++ pip_search/utils.py
@@ -1,21 +1,12 @@
-from sys import version_info as py_ver
+try:
+    from importlib.metadata import PackageNotFoundError, distribution
+except ImportError:
+    from pkg_resources import DistributionNotFound as PackageNotFoundError
+    from pkg_resources import get_distribution as distribution
 
-if py_ver[0] == 3 and py_ver[1] >= 8:
 
-	from importlib.metadata import PackageNotFoundError
-	from importlib.metadata import version as pkg_version
-
-
-	def check_version(package: str):
-		try:
-			return pkg_version(package)
-		except PackageNotFoundError:
-			return False
-
-else:
-	import pkg_resources as pkg
-	def check_version(package: str):
-		try:
-			return pkg.get_distribution(package).version
-		except :
-			return False
+def check_version(package: str):
+    try:
+        return distribution(package).version
+    except PackageNotFoundError:
+        return False
