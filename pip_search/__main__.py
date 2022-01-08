603ef420747cef102399e34d7823e192f5965d1e
Rodrigo Cristiano <rcristianofv@hotmail.com>
2022-01-08 07:35:39 -0300

    refactor: Allow programmatic use Add version opt

--- pip_search/__main__.py
+++ pip_search/__main__.py
@@ -1,19 +1,78 @@
-import sys
 import argparse
+import sys
+from urllib.parse import urlencode
+
+from rich.console import Console
+from rich.table import Table
+
+import pip_search
 from pip_search.pip_search import search
+
+from . import __version__
+from .utils import check_version
 
 
 def main():
-    ap = argparse.ArgumentParser()
-    ap.add_argument('-s', '--sort', type=str, const='name', nargs='?',
-                    choices=['name', 'version', 'released'],
-                    help='sort results by package name, version or \
-                        release date (default: %(const)s)')
-    ap.add_argument('query', nargs='+', type=str,
-                    help='terms to search pypi.org package repository')
+    ap = argparse.ArgumentParser(
+        prog="pip_search", description="Search for packages on PyPI"
+    )
+    ap.add_argument(
+        "-s",
+        "--sort",
+        type=str,
+        const="name",
+        nargs="?",
+        choices=["name", "version", "released"],
+        help="sort results by package name, version or \
+                        release date (default: %(const)s)",
+    )
+    ap.add_argument(
+        "query",
+        nargs="*",
+        type=str,
+        help="terms to search pypi.org package repository",
+    )
+    ap.add_argument(
+        "--version",
+        action="version",
+        version=f"%(prog)s {__version__}",
+    )
     args = ap.parse_args()
-    search(query=' '.join(args.query), opts=args)
+    query = " ".join(args.query)
+    result = search(query, opts=args)
+    if not args.query:
+        ap.print_help()
+        sys.exit(1)
+
+    table = Table(
+        title=(
+            "[not italic]:snake:[/] [bold][magenta]"
+            f"{pip_search.config.api_url}?{urlencode({'q': query})}"
+            "[/] [not italic]:snake:[/]"
+        )
+    )
+    table.add_column("Package", style="cyan", no_wrap=True)
+    table.add_column("Version", style="bold yellow")
+    table.add_column("Released", style="bold green")
+    table.add_column("Description", style="bold blue")
+    emoji = ":open_file_folder:"
+    for package in result:
+        checked_version = check_version(package.name)
+        if checked_version == package.version:
+            package.version = f"[bold cyan]{package.version} ==[/]"
+        elif checked_version is not False:
+            package.version = (
+                f"{package.version} > [bold purple]{checked_version}[/]"
+            )
+        table.add_row(
+            f"[link={package.link}]{emoji}[/link] {package.name}",
+            package.version,
+            package.released_date_str(),
+            package.description,
+        )
+    console = Console()
+    console.print(table)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
