ba7eebf513e4bbd050486b7e0d6666f7ed918eb0
kthordarson <kthordarson@gmail.com>
2022-09-03 00:19:43 +0200

    add github info and text output

--- pip_search/__main__.py
+++ pip_search/__main__.py
@@ -11,6 +11,46 @@ from . import __version__
 from .utils import check_version
 
 
+def text_output(result, query, args):
+    #res = [k for k in result]
+    for package in result:
+        if package.info_set:
+            print(f'{package.name} l:{package.link} ver:{package.version} rel:{package.released_date_str(args.date_format)} gh:{package.github_link} s:{package.stars} f:{package.forks} w:{package.watchers}')
+        else:
+            print(f'{package.name} l:{package.link} ver:{package.version} rel:{package.released_date_str(args.date_format)}')
+        print(f'\tdescription: {package.description}')
+
+def table_output(result, query, args):
+    table = Table(
+        title=(
+            "[not italic]:snake:[/] [bold][magenta]"
+            f"{config.api_url}?{urlencode({'q': query})}"
+            "[/] [not italic]:snake:[/]"
+        )
+    )
+    table.add_column("Package", style="cyan", no_wrap=True)
+    table.add_column("Version", style="bold yellow")
+    table.add_column("Released", style="bold green")
+    table.add_column("Description", style="bold blue")
+    emoji = ":open_file_folder:"    
+    for package in result:
+        checked_version = check_version(package.name)
+        if checked_version == package.version:
+            package.version = f"[bold cyan]{package.version} ==[/]"
+        elif checked_version is not False:
+            package.version = (
+                f"{package.version} > [bold purple]{checked_version}[/]"
+            )
+        table.add_row(
+            f"[link={package.link}]{emoji}[/link] {package.name}",
+            package.version,
+            package.released_date_str(args.date_format),
+            package.description,
+        )
+    console = Console()
+    console.print(table)
+
+
 def main():
     ap = argparse.ArgumentParser(
         prog="pip_search", description="Search for packages on PyPI"
@@ -43,41 +83,20 @@ def main():
         nargs="?",
         help="format for release date, (default: %(default)s)",
     )
+    ap.add_argument(
+        "--auth",
+        action="store_true",
+        help="use github authentication",
+    )
     args = ap.parse_args()
     query = " ".join(args.query)
     result = search(query, opts=args)
+    res = [k for k in result]
     if not args.query:
         ap.print_help()
         sys.exit(1)
+    text_output(res, query, args)
 
-    table = Table(
-        title=(
-            "[not italic]:snake:[/] [bold][magenta]"
-            f"{config.api_url}?{urlencode({'q': query})}"
-            "[/] [not italic]:snake:[/]"
-        )
-    )
-    table.add_column("Package", style="cyan", no_wrap=True)
-    table.add_column("Version", style="bold yellow")
-    table.add_column("Released", style="bold green")
-    table.add_column("Description", style="bold blue")
-    emoji = ":open_file_folder:"
-    for package in result:
-        checked_version = check_version(package.name)
-        if checked_version == package.version:
-            package.version = f"[bold cyan]{package.version} ==[/]"
-        elif checked_version is not False:
-            package.version = (
-                f"{package.version} > [bold purple]{checked_version}[/]"
-            )
-        table.add_row(
-            f"[link={package.link}]{emoji}[/link] {package.name}",
-            package.version,
-            package.released_date_str(args.date_format),
-            package.description,
-        )
-    console = Console()
-    console.print(table)
 
 
 if __name__ == "__main__":
--- pip_search/pip_search.py
+++ pip_search/pip_search.py
@@ -1,4 +1,6 @@
 import re
+import os
+from loguru import logger
 from argparse import Namespace
 from dataclasses import InitVar, dataclass
 from datetime import datetime
@@ -6,6 +8,7 @@ from typing import Generator, Union
 from urllib.parse import urljoin
 
 import requests
+from requests.auth import HTTPBasicAuth
 from bs4 import BeautifulSoup
 
 
@@ -30,6 +33,7 @@ class Package:
     version: str
     released: str
     description: str
+    info_set: bool = False
     link: InitVar[str] = None
 
     def __post_init__(self, link: str = None):
@@ -37,6 +41,10 @@ class Package:
         self.released_date = datetime.strptime(
             self.released, "%Y-%m-%dT%H:%M:%S%z"
         )
+        self.stars: str = ''
+        self.forks: str = ''
+        self.watchers: str = ''
+        self.github_link: str = ''
 
     def released_date_str(self, date_format: str = config.date_format) -> str:
         """Return the released date as a string formatted
@@ -46,6 +54,13 @@ class Package:
             str: Formatted date string
         """
         return self.released_date.strftime(date_format)
+
+    def set_gh_info(self, info):
+        self.info_set = True
+        self.stars = info['stars']
+        self.forks = info['forks']
+        self.watchers = info['watchers']
+        self.github_link = info['github_link']
 
 
 def search(
@@ -63,6 +78,11 @@ def search(
         r = s.get(config.api_url, params=params)
         soup = BeautifulSoup(r.text, "html.parser")
         snippets += soup.select('a[class*="package-snippet"]')
+    authparam = None
+    if 'auth' in opts:
+        GITHUBAPITOKEN = os.getenv('GITHUBAPITOKEN')
+        GITHUB_USERNAME = os.getenv('GITHUB_USERNAME')
+        authparam = HTTPBasicAuth(GITHUB_USERNAME, GITHUBAPITOKEN)
 
     if "sort" in opts:
         if opts.sort == "name":
@@ -110,4 +130,69 @@ def search(
             " ",
             snippet.select_one('p[class*="package-snippet__description"]').text.strip(),
         )
-        yield Package(package, version, released, description, link)
+        info = get_github_info(link, authparam)
+        pack = Package(package, version, released, description, link)
+        if info['set']:
+            pack.set_gh_info(info)
+        yield pack #Package(package, version, released, description, link, links)
+
+def get_repo_info(repo, auth=None):
+    info = {'stars':'', 'forks':'', 'watchers':'', 'set':False}    
+    if 'github' in repo:
+        try:
+            reponame = repo.split('github.com/')[1].rstrip('/')            
+        except IndexError as e:
+            logger.error(f'[r] err:{e} repo:{repo}')
+            return info
+        apiurl = f'https://api.github.com/repos/{reponame}'
+        r = requests.get(apiurl, auth=auth)
+        if 'Not Found' in r.text:
+            logger.warning(f'[r] {r.status_code} r:{reponame} apiurl:{apiurl} not found' )
+            return info
+        if 'API rate limit exceeded' in r.text:
+            logger.warning(f'[r] {r.status_code} {reponame} apiurl:{apiurl} API rate limit exceeded')
+            return info
+        try:
+            info['stars'] = str(r.json()["stargazers_count"])
+        except TypeError as e:
+            logger.error(f'[gri] {e} r:{r.status_code} len:{len(r.content)} apiurl:{apiurl} rj:{r.json()}')
+        info['forks'] = str(r.json()["forks_count"])
+        info['watchers'] = str(r.json()["watchers_count"])
+        info['github_link'] = repo
+        info['set'] = True
+        return info
+    else:
+        logger.warning(f'[gri] wrong url: {repo}')
+        return info
+
+def get_github_info(repolink, authparam):
+    gh_link = get_links(repolink)
+    logger.debug(f'[ggi] repolink:{repolink} gh:{gh_link}')
+    github_link = gh_link['github']
+    homepage = gh_link['homepage']
+    info = get_repo_info(github_link,authparam)
+    return info
+
+def get_links(pkg_url):
+    s = requests.session()
+    r = s.get(pkg_url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    homepage = ''
+    githublink = ''
+    docslink = ''
+    try:
+        homepage = soup.select_one('.vertical-tabs__tabs > div:nth-child(2) > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)',href=True).attrs['href']
+        if 'github' in homepage:
+            githublink = homepage
+            githublink = githublink.replace('/tags','')
+            return {'github':githublink, 'homepage':homepage}
+    except AttributeError as e:
+        logger.warning(f'[err] err:{e} pkg_url:{pkg_url}')
+        #logger.warning(f'[!] No homepage for {pkg_url}')
+    try:
+        githublink = soup.select_one('.vertical-tabs__tabs > div:nth-child(2) > ul:nth-child(2) > li:nth-child(2) > a:nth-child(1)',href=True).attrs['href']
+        githublink = githublink.replace('/tags','')
+        return {'github':githublink, 'homepage':homepage}
+    except AttributeError as e:
+        logger.warning(f'[err] err:{e} pkg_url:{pkg_url}')
+    return {'github':githublink, 'homepage':homepage}    
