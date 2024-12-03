c647017256ffed23df8de3e4a67bb4ecef0739d3
Victor Garric <victor.garric@gmail.com>
2024-12-03 14:00:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="pip_search",
-    version="0.0.12",
+    version="0.0.13",
     author="Victor Garric",
     author_email="victor.garric@gmail.com",
     url="https://github.com/victorgarric/pip_search",
